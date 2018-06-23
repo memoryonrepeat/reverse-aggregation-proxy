905bec6aad182e5ce5f656a8e55c032567aacd63
Thanh Nguyen <tienthanh8490@gmail.com>
2018-06-24 05:08:48 +0800

    basic server and return marshalled json

--- app/app.go
+++ app/app.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
@@ -11,20 +13,32 @@ import (
 )
 
 type Recipe struct {
-	Id          string
-	Name        string
-	Headline    string
-	Description string
-	Difficulty  int
-	PrepTime    string
-	ImageLink   string
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Headline    string `json:"headline"`
+	Description string `json:"description"`
+	Difficulty  int    `json:"difficulty"`
+	PrepTime    string `json:"prepTime"`
+	ImageLink   string `json:"imageLink"`
 	Ingredients []struct {
-		Name      string
-		ImageLink string
-	}
+		Name      string `json:"name"`
+		ImageLink string `json:"imageLink"`
+	} `json:"ingredients"`
 }
 
 func main() {
+	http.HandleFunc("/", ReverseAggregatorProxy)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func ReverseAggregatorProxy(w http.ResponseWriter, req *http.Request) {
+	r := fetch()
+	j, e := json.Marshal(r)
+	check(e)
+	io.WriteString(w, string(j))
+}
+
+func fetch() []Recipe {
 	// Specify timeout to avoid apps to hang unexpecedly since there is no timeout by default
 	// https://medium.com/@nate510/don-t-use-go-s-default-http-client-4804cb19f779
 	httpClient := &http.Client{
@@ -35,7 +49,7 @@ func main() {
 	timeout := time.After(2 * time.Second)
 	for i := 1; i < 5; i++ {
 		go (func(i int) {
-			c <- fetch("https://s3-eu-west-1.amazonaws.com/test-golang-recipes/"+strconv.Itoa(i), httpClient)
+			c <- fetchSingleRecipe("https://s3-eu-west-1.amazonaws.com/test-golang-recipes/"+strconv.Itoa(i), httpClient)
 		})(i)
 	}
 	for i := 0; i < 4; i++ {
@@ -44,15 +58,15 @@ func main() {
 			recipes = append(recipes, recipe)
 		case <-timeout:
 			fmt.Println("timeout")
-			return
+			// return recipes
 		}
 		/*recipe := <-c
 		recipes = append(recipes, recipe)*/
 	}
-	fmt.Println(recipes)
+	return recipes
 }
 
-func fetch(url string, client *http.Client) Recipe {
+func fetchSingleRecipe(url string, client *http.Client) Recipe {
 	fmt.Println(url)
 	req, err := http.NewRequest("GET", url, nil)
 	check(err)
